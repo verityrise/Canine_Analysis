ebac441b19f80cc15906d83a5e5f02de7229444a
verityrise <verityrise@gmail.com>
2015-12-02 14:20:48 +1100

    add integrate

---
+++ integrate_genome.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+'''
+This programs is to integrate dog reference genome from chr to a single one.
+Author: Hongzhi Luo
+'''
+import gzip
+import glob
+import shutil
+
+path='/vlsci/LSC0007/shared/canine_alport_syndrome/ref_files/'
+#path=''
+prefix='cfa_ref_CanFam3.1'
+
+def integrate_genome():
+    '''
+    @param: num: chr1...chrMT in the list.
+    '''
+    files=sorted(glob.glob(path+prefix+"*.fa.gz"))
+    #cat_together=[]
+    for f in files:
+        #cat_together.append(f)
+    #for files in cat_together:
+        outfile=gzip.open(path+prefix+".fa.gz",'wb')
+        for f in files:
+            gfile=gzip.open(f)
+            outfile.write(gfile.read())
+            gfile.close()
+        outfile.close()
+    
+if __name__ == '__main__':
+    integrate_genome()
